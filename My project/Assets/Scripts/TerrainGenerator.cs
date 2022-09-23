c817c8340b690c9bab542afe1d9bd5a397f153e1
Arthur <xxxr771@gmail.com>
2022-09-23 11:11:15 +0300

    lesson_2
    -terrain generator limited max block
    -delete overflow element in terrain generator
    -create start plane
    -update terrains in terrain generator

--- My project/Assets/Scripts/TerrainGenerator.cs
+++ My project/Assets/Scripts/TerrainGenerator.cs
@@ -6,29 +6,57 @@ public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField]
     private List<TerrainObject> terrainDatas = new List<TerrainObject>();
+    [SerializeField]
+    private int maxTerrains;
+    [SerializeField]
+    private int minDistanceCreating;
 
+    private List<GameObject> currentTerrains = new List<GameObject>();
     private Vector3 currentPosition = new Vector3(0,0,0);
-    void Start()
+    private void Start()
     {
-        
+        BuildStartPlane();
     }
 
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
+        {
+            UpdateTerrains();
+        }
+    }
+
+    private void BuildStartPlane()
+    {
+        for(int i = 0; i < minDistanceCreating; i++)
         {
             BuildTerrain();
         }
     }
-
     private void BuildTerrain()
     {
         int whichTerrain = Random.Range(0, terrainDatas.Count);
         int countsOfTerrain = Random.Range(1, terrainDatas[whichTerrain].maxInSuccession);
         for(int i = 0; i < countsOfTerrain; i++)
         {
-            Instantiate(terrainDatas[whichTerrain].terrain, currentPosition, Quaternion.identity);
+            var gameObject = Instantiate(terrainDatas[whichTerrain].terrain, currentPosition, Quaternion.identity);
+            currentTerrains.Add(gameObject);
             currentPosition.x++;
+            if(currentTerrains.Count > maxTerrains)
+            {
+                Destroy(currentTerrains[0]);
+                currentTerrains.RemoveAt(0);
+            }
+        }
+    }
+
+    private void UpdateTerrains()
+    {
+        BuildTerrain();
+        if (currentTerrains.Count > maxTerrains)
+        {
+            Destroy(currentTerrains[0]);
+            currentTerrains.RemoveAt(0);
         }
     }
 }
